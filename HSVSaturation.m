559dfe817671e0adb65b0c64b1b1c9a337d60620
FURNISS Remy <s2007428@ed.ac.uk>
2021-03-02 15:40:53 +0000

    saturation limits

--- HSVSaturation.m
+++ HSVSaturation.m
@@ -5,10 +5,12 @@ function images = HSVSaturation(images,sd)
         imageHSV = rgb2hsv(image);
         saturation = imageHSV(:,:,2);
         mat = normrnd(0,sd,224,224);
-        imageHSV(:,:,2) = saturation + mat; 
+        newSat = saturation + mat;
+        newSat(newSat<0)=0;
+        newSat(newSat>1)=1;
+        imageHSV(:,:,2) = newSat; 
         images(:,:,:,n) = hsv2rgb(imageHSV);
     end
 
     images = uint8(images);
 end
-
