28c953d876a98306f51c875199cd4ad51b917289
FURNISS Remy <s2007428@ed.ac.uk>
2021-03-02 15:54:37 +0000

    Hue limits

--- HSVNoise.m
+++ HSVNoise.m
@@ -5,7 +5,9 @@ function images = HSVNoise(images,sd)
         imageHSV = rgb2hsv(image);
         hue = imageHSV(:,:,1);
         mat = normrnd(0,sd,224,224);
-        imageHSV(:,:,1) = hue + mat; 
+        newHue = hue + mat;
+        newHue(newHue>1) = newHue(newHue>1)-1;
+        imageHSV(:,:,1) = newHue; 
         images(:,:,:,n) = hsv2rgb(imageHSV);
     end
 
