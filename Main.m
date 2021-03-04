01f5935b9bd354c2c42eeb8cfc9929a7186ce640
FURNISS Remy <s2007428@ed.ac.uk>
2021-03-04 20:01:41 +0000

    typo

--- Main.m
+++ Main.m
@@ -37,7 +37,7 @@ labels = {testALabels,testBLabels,testCLabels};
 [bagC,svmC] = bagOfWordsTrain(trainC);
 
 bags = {bagA,bagB,bagC};
-svms = {svmA,svmB,svmV};
+svms = {svmA,svmB,svmC};
 
 %Train ResNet18 classifier
 
@@ -51,34 +51,34 @@ nets = {netA,netB,netC};
 
 save nets bags svms labels imgs;
 
-%load nets bags svams labels imgs;
-
-%clearvars -except nets bags svms images labels;
-
-%Vars
-
-gNoiseSD = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
-gBlurTimes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-cIncSD = [1.0, 1.03, 1.06, 1.09, 1.12, 1.15, 1.18, 1.21, 1.24, 1.27];
-
-cDecSD = [1.0, 0.90, 0.80, 0.70, 0.60, 0.50, 0.40, 0.30, 0.20, 0.10];
-bIncDecSD = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45];
-
-hueSatSD = [ 0.00, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18];
-oLen = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45 ];
-
-%Tests
-
-% RUN ONE AT A TIME
-
-% runTest(bags,svms,nets,images,labels,gNoiseSD,@gaussianNoise,'Gaussian Noise');
-% runTest(bags,svms,nets,images,labels,gBlurTimes,@gaussianBlur,'Gaussian Blur');
-% runTest(bags,svms,nets,images,labels,cIncSD,@imageContrast,'Contrast Increase');
+% %load nets bags svams labels imgs;
 % 
-% runTest(bags,svms,nets,images,labels,cDecSD,@imageContrast,'Contrast Decrease');
-% runTest(bags,svms,nets,images,labels,bIncDecSD,@imageBrightness,'Brightness Increase');
-% runTest(bags,svms,nets,images,labels,-bIncDecSD,@imageBrightness,'Brightness Decrease');
+% %clearvars -except nets bags svms images labels;
 % 
-% runTest(bags,svms,nets,images,labels,hueSatSD,@HSVHue,'Hue Noise');
-% runTest(bags,svms,nets,images,labels,hueSatSD,@HSVSaturation,'Saturation Noise');
-% runTest(bags,svms,nets,images,labels,oLen,@occlusion,'Occlusion');
+% %Vars
+% 
+% gNoiseSD = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+% gBlurTimes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+% cIncSD = [1.0, 1.03, 1.06, 1.09, 1.12, 1.15, 1.18, 1.21, 1.24, 1.27];
+% 
+% cDecSD = [1.0, 0.90, 0.80, 0.70, 0.60, 0.50, 0.40, 0.30, 0.20, 0.10];
+% bIncDecSD = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45];
+% 
+% hueSatSD = [ 0.00, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18];
+% oLen = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45 ];
+% 
+% %Tests
+% 
+% % RUN ONE AT A TIME
+% 
+% % runTest(bags,svms,nets,images,labels,gNoiseSD,@gaussianNoise,'Gaussian Noise');
+% % runTest(bags,svms,nets,images,labels,gBlurTimes,@gaussianBlur,'Gaussian Blur');
+% % runTest(bags,svms,nets,images,labels,cIncSD,@imageContrast,'Contrast Increase');
+% % 
+% % runTest(bags,svms,nets,images,labels,cDecSD,@imageContrast,'Contrast Decrease');
+% % runTest(bags,svms,nets,images,labels,bIncDecSD,@imageBrightness,'Brightness Increase');
+% % runTest(bags,svms,nets,images,labels,-bIncDecSD,@imageBrightness,'Brightness Decrease');
+% % 
+% % runTest(bags,svms,nets,images,labels,hueSatSD,@HSVHue,'Hue Noise');
+% % runTest(bags,svms,nets,images,labels,hueSatSD,@HSVSaturation,'Saturation Noise');
+% % runTest(bags,svms,nets,images,labels,oLen,@occlusion,'Occlusion');
