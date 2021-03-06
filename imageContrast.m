a33878f138a81bda514ea14057113bd14cfb5e65
Wojciech Mista <wojciech.mista@hotmail.com>
2021-03-07 00:08:32 +0100

    unfix

--- imageContrast.m
+++ imageContrast.m
@@ -3,10 +3,6 @@ function images = imageContrast(images, val)
     for n = 1:size(images,4)
         image = images(:,:,:,n);
         images(:,:,:,n) = image * val;
-
-        if images(:,:,:,n) > 255
-            images(:,:,:,n) = 255;
-        end
     end
 
     images = uint8(images);
